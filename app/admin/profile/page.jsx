7c35ddd8414510ff23a3f3c6e274afc6c8b3042b
Envobyte Jr Dev <kakon-ray>
2024-05-07 15:56:39 +0600

    Profile page add

--- app/admin/profile/page.jsx
+++ app/admin/profile/page.jsx
@@ -33,11 +33,44 @@ const page = () => {
     return (
         <main className="flex min-h-screen flex-col">
         <Navbar/>
-        <hr />
-            <h1>This is User Profile Page</h1>
-            <h2>{user.name}</h2>
-            <h2>{user.email}</h2>
 
+  
+<div class="flex items-center h-screen w-full justify-center">
+
+<div class="max-w-xs">
+    <div class="bg-white shadow-xl rounded-lg py-3">
+        <div class="photo-wrapper p-2">
+            <img class="w-32 h-32 rounded-full mx-auto" src={user.image} alt="John Doe"/>
+        </div>
+        <div class="p-2">
+            <h3 class="text-center text-xl text-gray-900 font-medium leading-8">{user.name}</h3>
+            <div class="text-center text-gray-400 text-xs font-semibold">
+                <p>Web Developer</p>
+            </div>
+            <table class="text-xs my-3">
+                <tbody><tr>
+                    <td class="px-2 py-2 text-gray-500 font-semibold">Address</td>
+                    <td class="px-2 py-2">Chatakpur-3, Dhangadhi Kailali</td>
+                </tr>
+                <tr>
+                    <td class="px-2 py-2 text-gray-500 font-semibold">Phone</td>
+                    <td class="px-2 py-2">+88 {user.phone}</td>
+                </tr>
+                <tr>
+                    <td class="px-2 py-2 text-gray-500 font-semibold">Email</td>
+                    <td class="px-2 py-2">{user.email}</td>
+                </tr>
+            </tbody></table>
+
+            <div class="text-center my-3">
+                <a class="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium" href="#">View Profile</a>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+</div>
 
         </main>
     );
