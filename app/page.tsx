547d12dcf98ced9faddb4b369e97e1523fc54379
Envobyte Jr Dev <kakon-ray>
2024-05-07 18:34:49 +0600

    Product Name Add

--- app/page.tsx
+++ app/page.tsx
@@ -43,6 +43,7 @@ export default function Home() {
             <div className="max-w-sm rounded overflow-hidden shadow-lg">
               <img className="w-full" src={item.product_img} alt="Sunset in the mountains" />
               <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{item.product_name}</div>
                 <div className="font-bold text-xl mb-2">{item.product_number}</div>
                 <p className="text-gray-700 text-base">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
