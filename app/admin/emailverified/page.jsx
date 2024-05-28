cff69cad226791384a1db6201bcc57397a58d498
Envobyte Jr Dev <kakon-ray>
2024-05-28 10:15:32 +0600

    Error Hanlding

--- app/admin/emailverified/page.jsx
+++ app/admin/emailverified/page.jsx
@@ -1,49 +1,47 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client"
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import axios from 'axios';
 import Link from 'next/link';
 
 const EmailVerified = () => {
+    const [response,setResponse] = useState('')
     const searchParams = useSearchParams()
     const token = searchParams.get('token')
     const email = searchParams.get('email')
 
 
-    const handleDelete = async () => {
-
+    const handleRequest = async () => {
         try {
             const response = await axios.post('http://192.168.5.239:8000/api/email-verified', {
                 token,
                 email
             });
 
-            console.log(response);
+            setResponse(response.data.success)
 
         } catch (error) {
             console.log(error);
 
         }
-
-
-
     };
 
 
     useEffect(() => {
+        handleRequest()
+    },[]);
 
-        handleDelete()
-
-    }, []);
 
 
 
     return (
         <main className="flex min-h-screen flex-col">
-            <div className="block text-center max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mx-auto mt-10">
+            {response ? <div className="block text-center max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mx-auto mt-10">
                 <h1 className='text-xl text-purple-500 py-4'>Your Email is Verified</h1>
                  <Link href="http://localhost:3000/admin/login">Login Your Account</Link>
-            </div>
+            </div> : ""}
+            
         </main>
     );
 };
