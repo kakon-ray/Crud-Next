e16174d0ee8e0163dc348d7d5472364239ed3c91
Envobyte Jr Dev <kakon-ray>
2024-06-06 12:09:07 +0600

    Text Chnage

--- app/admin/passwordreset/resetform/page.jsx
+++ app/admin/passwordreset/resetform/page.jsx
@@ -78,7 +78,7 @@ const page = () => {
 
                         <div className="flex items-center justify-between">
                             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                                Sign up
+                                Reset Password
                             </button>
 
                         </div>
--- app/admin/passwordreset/sendemail/page.jsx
+++ app/admin/passwordreset/sendemail/page.jsx
@@ -51,7 +51,7 @@ const page = () => {
 
                         <div className="flex items-center justify-between">
                             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                                Sign up
+                                Send Email
                             </button>
 
                         </div>
